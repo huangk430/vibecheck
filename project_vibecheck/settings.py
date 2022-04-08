68109d9884e11a2603ba37f6f07377046adbbe26
huangk430 <64836972+huangk430@users.noreply.github.com>
2022-04-08 09:20:57 -0700

    using environment variable for secret key

--- project_vibecheck/settings.py
+++ project_vibecheck/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i_do%-6+^^p12rh*+4k7%h^5buvun6@d%((%c0f!r&!8u)@wvk'
+SECRET_KEY = os.getenv("SECRET_KEY") or 'django-insecure-i_do%-6+^^p12rh*+4k7%h^5buvun6@d%((%c0f!r&!8u)@wvk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
