eb188c5286e8c972380d0804cf48d1105ffaf4da
huangk430 <64836972+huangk430@users.noreply.github.com>
2022-05-17 09:50:11 -0700

    All backend functions complete, frontend in progress

--- vibecheck/views.py
+++ vibecheck/views.py
@@ -1,10 +1,16 @@
 import random
+import os
+import requests
+import json
+from django.utils import timezone as tz
+from django.contrib import messages
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from project_vibecheck.settings import LOGIN_REDIRECT_URL
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+import spotipy.util as util
 from allauth.socialaccount.models import SocialAccount
 from vibecheck.models import *
 
@@ -24,9 +30,8 @@ def index(request):
 @login_required
 def show_playlist(request, vibeid):
     #creating the playlist
+    v = get_object_or_404(Vibe, id=vibeid)
     if request.method != "POST":
-        # v = Vibe.objects.get(id=vibeid)
-        v = get_object_or_404(Vibe, id=vibeid)
         vibeGenres = list(v.genres.all().values_list("name", flat=True))
         scope = "user-library-read"
         context = {"tracks": []}
@@ -44,23 +49,47 @@ def show_playlist(request, vibeid):
         seedTracks = random.sample(savedTrackIDs, k=3)
         seedGenres = random.sample(vibeGenres, k=2)
 
-        context["seedTracks"] = [f"{idx} {item['track']['artists'][0]['name']} –  {item['track']['name']}<br>" for idx,item in enumerate(savedTracks["items"]) if item["track"]["id"] in seedTracks]
+        context["seedTracks"] = [f"{item['track']['artists'][0]['name']} –  {item['track']['name']}" for item in savedTracks["items"] if item["track"]["id"] in seedTracks]
         context["seedGenres"] = seedGenres
         results = sp.recommendations(seed_tracks=seedTracks, seed_genres=seedGenres)
         request.session["playlist"] = results
         
 
-        for idx, track in enumerate(results['tracks']):
-            context["tracks"].append(f"{idx} {track['artists'][0]['name']} –  {track['name']}<br>")
+        for track in results['tracks']:
+            context["tracks"].append(f"{track['artists'][0]['name']} –  {track['name']}")
     
         return render(request, "vibecheck/show_playlist.html", context)
         
     #save playlist on spotify
     else:
-        scope = "playlist-modify-public"
-        sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
         user_id = SocialAccount.objects.get(user=request.user).uid
-        sp.user_playlist_create(user_id, "Test Playlist", description="Created by VibeCheck")
+        scope = "playlist-modify-public"
+        # sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+        # sp.user_playlist_create(user_id, "Test Playlist", description="Created by VibeCheck")
+        CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
+        CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
+        REDIRECT_URI = os.getenv("SPOTIPY_REDIRECT_URI")
+        token = util.prompt_for_user_token(user_id, scope=scope, client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI)
+        endpoint_url = f"https://api.spotify.com/v1/users/{user_id}/playlists"
+        playlist_name = f"{v.name.capitalize()} Playlist {tz.now().month}/{tz.now().day}"
+        request_body = json.dumps({
+            "name": playlist_name,
+            "description": "Created by VibeCheck",
+            "public": True # let's keep it between us - for now
+        })
+        response = requests.post(url = endpoint_url, data = request_body, headers={"Content-Type":"application/json", "Authorization":f"Bearer {token}"})
+        playlist_id = response.json()['id']
+
+        
+        endpoint_url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
+        request_body = json.dumps({
+                "uris": [request.session["playlist"]["tracks"][i]["uri"] for i in range(len(request.session["playlist"]["tracks"]))]
+                })
+        response = requests.post(url = endpoint_url, data = request_body, headers={"Content-Type":"application/json", 
+                                "Authorization":f"Bearer {token}"})
+
+        messages.success(request, f'Your playlist was saved successfully as "{playlist_name}"')
+
         #redirect back to home page
         return redirect("index")
 
